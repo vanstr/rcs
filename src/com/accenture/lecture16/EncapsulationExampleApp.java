5d1b1f78154dd63b7ee95b170de975dcbbe04923
Ivan Mihnenok <mihnenok@gmail.com>
2019-11-24 16:11:30 +0200

    lecture16 added access modifiers

--- src/com/accenture/lecture16/EncapsulationExampleApp.java
+++ src/com/accenture/lecture16/EncapsulationExampleApp.java
@@ -1,10 +1,35 @@
 package com.accenture.lecture16;
 
 
+class BankAccount {
+	private String owner;
+	private int balance;
+	
+	
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	
+	
+}
+
 public class EncapsulationExampleApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		BankAccount johnAccount = new BankAccount();
+		johnAccount.setOwner("John");
+		johnAccount.setBalance(100); 
 
 	}
 

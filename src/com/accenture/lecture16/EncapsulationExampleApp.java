dd68bae9b65f97a4f7007cbe19798ee7e56afa82
Ivan Mihnenok <mihnenok@gmail.com>
2019-11-24 18:31:52 +0200

    BankAccount created

--- src/com/accenture/lecture16/EncapsulationExampleApp.java
+++ src/com/accenture/lecture16/EncapsulationExampleApp.java
@@ -1,10 +1,18 @@
 package com.accenture.lecture16;
 
 
+class BankAccount {
+	String owner;
+	int balance;
+
+}
+
 public class EncapsulationExampleApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BankAccount johnAccount = new BankAccount();
+		johnAccount.owner = "John";
+		johnAccount.balance = 100; 
 
 	}
 

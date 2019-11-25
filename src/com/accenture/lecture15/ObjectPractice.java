b4c6834caaec3adbea6c33461877083c51d90bce
Raimonds Bortascenoks <r.bortascenoks@gmail.com>
2019-11-25 17:53:05 +0200

    lecture 15 impl

--- src/com/accenture/lecture15/ObjectPractice.java
+++ src/com/accenture/lecture15/ObjectPractice.java
@@ -6,23 +6,42 @@ public class ObjectPractice {
 
 	public static void main(String[] args) {
 
+		Weather estonia = new Weather("Estonia", new int[] { -3, 0, 6, 3, 6, 7, 8 });
+		Weather norway = new Weather("Norway", new int[] { -11, 10, 9, 9, 7, -2, 6 });
+		
+		Weather latvia = new Weather("Latvia", new int[] { -1, 0, 5, 2, 6, -1, 0 });
+		System.out.println(latvia.getCountry() + " " + Arrays.toString(latvia.getDaysWeather()));
+		System.out.println(latvia);
+		System.out.println("Average temperature in Riga will be " + latvia.getWeekAverage());
+		System.out.println("Highest temperature in Riga will be " + latvia.getHottestTemperature());
+		System.out.println("Lowest temperature in Riga will be" +latvia.getColdestTemperature()); 
+		System.out.println("Hottest day in Riga will be " + latvia.getHottestDay());
+		System.out.println("Hottest day in Riga will be " + latvia.getHottestDay2());
+		latvia.printAdvancedInformation();
+		latvia.applyWarmCycloneEffect();
+		System.out.println(latvia);
 
+		
 		// Task - Lets make our Weather class to store following information:
 		// String country - for which forecast is prepared.
 		// int[] daysWeather- temperature information for next week (7 days).
 
-		// Task - We should be able to create a new object of class Weather by passing
+		// Task - We should be able to create a new object of class Weather by
+		// passing
 		// both field values.
 		// Create constructor in Weather class
 		// public Weather(String country , int[] daysWeater)
 
-		// Task - We should prohibit to reassign country field value for weather objects
+		// Task - We should prohibit to reassign country field value for weather
+		// objects
 		// Make it final
 
-		// Task - Lets create few weather forecasts for following countries: Latvia,
+		// Task - Lets create few weather forecasts for following countries:
+		// Latvia,
 		// Antaliya, Bergen
 		// Example:
-		// Weather latvia = new Weather("Latvia", new int[]{-2, 0, 5, 2, 6, -1, 0});
+		// Weather latvia = new Weather("Latvia", new int[]{-2, 0, 5, 2, 6, -1,
+		// 0});
 
 		// Task - We should prohibit direct access to Weather object fields
 		// Make fields private
@@ -31,92 +50,112 @@ public class ObjectPractice {
 		// System.out.println(latvia.getCountry() + " "
 		// +Arrays.toString(latvia.getDaysWeather()));
 
-		// Task - User should be able to get weather information in user-friendly way
+		// Task - User should be able to get weather information in
+		// user-friendly way
 		// Create toString method in Weather class
-		// Output: "Weather forecast in 'Latvia' for next days '-2, 0, 5, 2, 6, -1, 0'"
+		// Output:
+		// "Weather forecast in 'Latvia' for next days '-2, 0, 5, 2, 6, -1, 0'"
 		// System.out.println(latvia);
 
 		// Send a link of your github repository to lecturer in slack
 		// Wait until everyone will reach that point
 		// -------------------------------------------------------------------------------------
-		
-// Task 7 - Create a method in class Weather which will return average temperature for days
-// method signature: public double getWeekAverage()
-// Output: Average temperature in Latvia will be 1.8
-// System.out.println("Average temperature in Latvia will be "+latvia.getWeekAverage());
 
+		// Task 7 - Create a method in class Weather which will return average
+		// temperature for days
+		// method signature: public double getWeekAverage()
+		// Output: Average temperature in Latvia will be 1.8
+		// System.out.println("Average temperature in Latvia will be "+latvia.getWeekAverage());
 
-// Task 8 - Create a method in class Weather which will return highest temperature		
-// Output: Highest temperature in Latvia will be 6
-// System.out.println("Highest temperature in Latvia will be" + latvia.getHottestTemperature());
+		// Task 8 - Create a method in class Weather which will return highest
+		// temperature
+		// Output: Highest temperature in Latvia will be 6
+		// System.out.println("Highest temperature in Latvia will be" +
+		// latvia.getHottestTemperature());
 
+		// Task 9 - Create a method in class Weather which will return hottest
+		// day name, lets assume:
+		// - first day of week is Monday
+		// - there will be 7 days forecasts only
+		// Use switch statement in order to solve it
+		// Output: Hottest day in Latvia will be Friday
+		// System.out.println("Hottest day in Latvia will be" +
+		// latvia.getHottestDayName());
 
-// Task 9 - Create a method in class Weather which will return hottest day name, lets assume:
-// - first day of week is Monday	
-// - there will be 7 days forecasts only 
-// Use switch statement in order to solve it
-// Output: Hottest day in Latvia will be Friday
-// System.out.println("Hottest day in Latvia will be" + latvia.getHottestDayName());
+		// Task 10 - Create a method in class Weather which will return highest
+		// temperature
+		// Output: Lowest temperature in Latvia will be -2
+		// System.out.println("Lowest temperature in Latvia will be" +
+		// latvia.getColdestTemperature());
 
+		// Task 11 - Create a method in class Weather which will return coldest
+		// day name:
+		// Output: Hottest day in Latvia will be Monday
+		// System.out.println("Hottest day in Latvia will be" +
+		// latvia.getColdestDayName());
 
-// Task 10 - Create a method in class Weather which will return highest temperature		
-// Output: Lowest temperature in Latvia will be -2
-// System.out.println("Lowest temperature in Latvia will be" + latvia.getColdestTemperature());				
+		// Task 12 - User wants to get advanced information printed out into
+		// console about weather in specific country
+		// Create method printAdvancedInformation() in Weather class
+		// Output:
+		// "In 'Latvia' during next 7 day will be following weather '-2, 0, 5,
+		// 2, 6, -1, 0',
+		// the highest temperature will be on Friday +6 degree,
+		// the lowest temperature will be on Monday -2 degree"
+		// latvia.printAdvancedInformation();
 
-// Task 11 - Create a method in class Weather which will return coldest day name:
-// Output: Hottest day in Latvia will be Monday
-// System.out.println("Hottest day in Latvia will be" + latvia.getColdestDayName()); 
-		
-
-// Task 12 - User wants to get advanced information printed out into console about weather in specific country
-// Create method printAdvancedInformation() in Weather class
-// Output: 
-// "In 'Latvia' during next 7 day will be following weather '-2, 0, 5, 2, 6, -1, 0', 
-// the highest temperature will be on Friday +6 degree,
-// the lowest temperature will be on Monday -2 degree"
-// latvia.printAdvancedInformation();
-
-// Task 13 - we need possibility to update weather temperature based on coming warm cyclone, and increase weather temperature for each day by one degree
-// create a public void applyWarmCycloneEffect()
-// Output: "Weather forecast  in 'Latvia' for next days '-1, 1, 6, 3, 7, 0, 1'"
-// latvia.applyWarmCycloneEffect();
-// System.out.println(latvia);
+		// Task 13 - we need possibility to update weather temperature based on
+		// coming warm cyclone, and increase weather temperature for each day by
+		// one degree
+		// create a public void applyWarmCycloneEffect()
+		// Output:
+		// "Weather forecast  in 'Latvia' for next days '-1, 1, 6, 3, 7, 0, 1'"
+		// latvia.applyWarmCycloneEffect();
+		// System.out.println(latvia);
 
 		// ------------------------------------------------------------------------------------------------
-						
-// Task - I want to be able to convert Celsius temperature into Fahrenheit
-// create a static method in Weather class: public static double celsiusToFahrenheit(double celsiusTemperature)
-// Output: 10 in Celsius will be 50 in Fahrenheit 
-// System.out.println("10 in Celsius will be "+ Weather.celsiusToFahrenheit(10) +" in Fahrenheit");		
 
+		// Task - I want to be able to convert Celsius temperature into
+		// Fahrenheit
+		// create a static method in Weather class: public static double
+		// celsiusToFahrenheit(double celsiusTemperature)
+		// Output: 10 in Celsius will be 50 in Fahrenheit
+		// System.out.println("10 in Celsius will be "+
+		// Weather.celsiusToFahrenheit(10) +" in Fahrenheit");
 
-// Task - We need that some one would provide to us weather forecast for specific country
-// - create a static method which will create a 7 day forecast for specific country
-//   public static Weather getWeatherForecast(String countryName)		
-// - google for random value generator, in order to fill day weather with some values
-// Example:
-// Weather lithuania = Weather.getWeatherForecast("Lithuania");
+		// Task - We need that some one would provide to us weather forecast for
+		// specific country
+		// - create a static method which will create a 7 day forecast for
+		// specific country
+		// public static Weather getWeatherForecast(String countryName)
+		// - google for random value generator, in order to fill day weather
+		// with some values
+		// Example:
+		// Weather lithuania = Weather.getWeatherForecast("Lithuania");
 
+		// Task - I as a worker of weather forecast center want to be able to
+		// overview and interact with weather forecasts
+		// - create a new class WeatherForecastApp in package
+		// com.accentrue.lecture15
+		// - create a predefined list of weather forecast, at least for 3
+		// countries and store them in ArrayList
+		// - User should be interact through console and should be able to:
+		// -- [l] - list country names for which forecast exist
+		// -- [a] - add new forecast, by providing country name and one day
+		// forecast temperature
+		// -- [d] - delete forecast, by specifying country name
+		// -- [g] - generate new weather forecast, by specifying country name
+		// -- [q] - quit the program
 
-// Task - I as a worker of weather forecast center want to be able to overview and interact with weather forecasts
-// - create a new class WeatherForecastApp in package com.accentrue.lecture15
-// - create a predefined list of weather forecast, at least for 3 countries and store them in ArrayList
-// - User should be interact through console and should be able to:
-// -- [l] - list country names for which forecast exist
-// -- [a] - add new forecast, by providing country name and one day forecast temperature       
-// -- [d] - delete forecast, by specifying country name
-// -- [g] - generate new weather forecast, by specifying country name
-// -- [q] - quit the program
+		// Extra task - make getHottestDayName() work with 7+ days forecast
 
+		// Extra Task - Add information about wind for each day
 
-// Extra task - make getHottestDayName() work with 7+ days forecast
+		// Extra Task - Represent day temperature by dayMax and dayMin, same for
+		// wind
+		// Hint: think about representing weather for specific day as another
+		// Object which will have fields, minTemp, maxTemp, minWind, maxWind
 
-// Extra Task - Add information about wind for each day 
-
-// Extra Task - Represent day temperature by dayMax and dayMin, same for wind
-// Hint: think about representing weather for specific day as another Object which will have fields, minTemp, maxTemp, minWind, maxWind
-  
-		
 	}
 
 }
--- src/com/accenture/lecture15/Weather.java
+++ src/com/accenture/lecture15/Weather.java
@@ -1,19 +1,125 @@
 package com.accenture.lecture15;
 
+import java.util.Arrays;
+
 public class Weather {
-	private final String country; 
-	private  int[] daysWeather; 
-	
-	public Weather(String country , int[] daysWeater) {
+
+	private final String country;
+	private int[] daysWeather;
+	private String[] weekWeather = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+	public Weather(String country, int[] daysWeather) {
+		super();
 		this.country = country;
-		this.daysWeather = daysWeater;
+		this.daysWeather = daysWeather;
+	}
+
+	public int[] getDaysWeather() {
+		return daysWeather;
 	}
 
 	public String getCountry() {
 		return country;
 	}
 
-	public int[] getDaysWeather() {
-		return daysWeather;
+	public String toString() {
+		return "Weather in " + getCountry() + " for next days " + "'" + Arrays.toString(daysWeather) + "'";
 	}
+
+	public double getWeekAverage() {
+		double sum = 0;
+		for (int i = 0; i < daysWeather.length; i++) {
+			sum += daysWeather[i];
+		}
+		double average = sum / daysWeather.length;
+		return average;
+	}
+
+	public String getHottestDay2() {
+		int hottestIndex = getHottestTemperatureIndex();
+		return weekWeather[hottestIndex];
+	}
+
+	public int getHottestTemperature() {
+		int hottestIndex = getHottestTemperatureIndex();
+		return daysWeather[hottestIndex];
+	}
+
+	private int getHottestTemperatureIndex() {
+		int maxWeather = Integer.MIN_VALUE;
+		int maxIndex = Integer.MIN_VALUE;
+		for (int i = 0; i < daysWeather.length; i++) {
+			if (daysWeather[i] > maxWeather) {
+				maxWeather = daysWeather[i];
+				maxIndex = i;
+			}
+		}
+		return maxIndex;
+	}
+
+	public String getHottestDay() {
+		int hottestIndex = getHottestTemperatureIndex();
+		switch (hottestIndex) {
+		case 0:
+			return "Monday";
+		case 1:
+			return "Tuesday";
+		case 2:
+			return "Wednsday";
+		case 3:
+			return "Thursday";
+		case 4:
+			return "Friday";
+		case 5:
+			return "Saturday";
+		case 6:
+			return "Sunday";
+		default:
+			return "Unknown";
+		}
+	}
+
+	private int getColdestTemperatureIndex() {
+		int minWeather = Integer.MAX_VALUE;
+		int minIndex = Integer.MAX_VALUE;
+		for (int i = 0; i < daysWeather.length; i++) {
+			if (daysWeather[i] < minWeather) {
+				minWeather = daysWeather[i];
+				minIndex = i;
+			}
+		}
+
+		return minIndex;
+	}
+
+	public int getColdestTemperature() {
+		int coldestIndex = getColdestTemperatureIndex();
+		return daysWeather[coldestIndex];
+
+	}
+
+	public String getColdestDay() {
+		int coldestIndex = getColdestTemperatureIndex();
+		return weekWeather[coldestIndex];
+	}
+
+	public void printAdvancedInformation() {
+		String hottestDay = getHottestDay2();
+		String coldestDay = getColdestDay();
+		int coldestTemp = getColdestTemperature();
+		int hottestTemp = getHottestTemperature();
+		System.out.println(
+				"In " + country + " during next 7 days will be folowing weather" + Arrays.toString(daysWeather));
+		System.out.println("The highest temperature will be on " + hottestDay + " " + hottestTemp + " degree.");
+		System.out.println("The lowest temperature will be on " + coldestDay + " " + coldestTemp + " degree.");
+	}
+
+	public void applyWarmCycloneEffect() {
+
+		for (int i = 0; i < daysWeather.length; i++) {
+			daysWeather[i]++;
+		}
+		System.out.println("Weather forecast in " + country + " for next days " + Arrays.toString(daysWeather));
+	}
+
 }

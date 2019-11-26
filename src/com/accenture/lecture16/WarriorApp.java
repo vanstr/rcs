aefc6407a3cb3a0109de6dbc7feae4331b5f1c89
Ivan Mihnenok <mihnenok@gmail.com>
2019-11-26 19:48:37 +0200

    Warrior example

---
+++ src/com/accenture/lecture16/WarriorApp.java
@@ -0,0 +1,15 @@
+package com.accenture.lecture16;
+
+public class WarriorApp {
+
+	public static void main(String[] args) {
+
+		Warrior john = new Warrior ("john", 88, 22);
+		Warrior peter = new Warrior ("peter", 65, 13);
+	
+		
+		
+		john.fight(peter);
+
+	}
+}

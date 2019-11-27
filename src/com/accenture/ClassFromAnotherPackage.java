eb2ff2bfc01e8a7ba9398a939313c5e08e0cf78f
Raimonds Bortascenoks <r.bortascenoks@gmail.com>
2019-11-27 18:28:28 +0200

    lecture 16 packaging task

---
+++ src/com/accenture/ClassFromAnotherPackage.java
@@ -0,0 +1,5 @@
+package com.accenture;
+
+public class ClassFromAnotherPackage {
+
+}
---
+++ src/com/accenture/lecture16/MyPackageApp.java
@@ -0,0 +1,12 @@
+package com.accenture.lecture16;
+
+import com.accenture.ClassFromAnotherPackage;
+
+public class MyPackageApp {
+
+	public static void main(String[] args) {
+		new ClassFromAnotherPackage();
+
+	}
+
+}

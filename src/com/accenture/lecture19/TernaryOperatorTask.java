a978bc7ccbf0210260ece879a7a1e5f6396c6cca
Ivan Mihnenok <mihnenok@gmail.com>
2019-11-30 11:22:40 +0200

    Nested ternary operator with

--- src/com/accenture/lecture19/TernaryOperatorTask.java
+++ src/com/accenture/lecture19/TernaryOperatorTask.java
@@ -6,13 +6,10 @@ public class TernaryOperatorTask {
 		// receive passed arguments and parse them from string to integers 
 		int arg1 = Integer.parseInt(args[0]);
 		int arg2 = Integer.parseInt(args[1]);
+		int arg3 = Integer.parseInt(args[3]);
 		
-		int biggestValue;
-		if(arg1 > arg2) {
-			biggestValue = arg1;
-		}else {
-			biggestValue = arg2;
-		}
-		System.out.println("The biggest value is " + biggestValue);
+		
+		int big = arg1 > arg2 ? (arg1 > arg3 ? arg1 : arg3) : (arg2 > arg3 ? arg2 : arg3) ;
+		System.out.println("The biggest value is " + big);
 	}
 }
